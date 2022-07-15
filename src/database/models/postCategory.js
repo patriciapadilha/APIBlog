48d5ba7c3b2b48896c1956ef1b6586ed84f44a62
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-07-15 09:13:25 -0300

    feat (blogPostCategory): Desenvolvimento da model da tabela de relacionamento entre as entidades

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const postCategorySchema = (sequelize, DataTypes) => {
+  const postCategoriesTable = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      references: {
+          model: 'BlogPost',
+          key: 'id'
+      }
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      references: {
+          model: 'Category',
+          key: 'id'
+      }
+    }
+  }, {
+      timestamps: false,
+      tableName: 'PostCategories',
+  });
+
+  postCategoriesTable.associate = (models) => {
+      models.BlogPost.belongsToMany(models.Category, {
+        foreignKey: 'categoryId',
+        as: 'Categories',
+        through: postCategoriesTable,
+        otherKey: 'postId',
+      });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      foreignKey: 'postId',
+      as: 'BlogPosts',
+      through: postCategoriesTable,
+      otherKey: 'categoryId',
+    });
+  }
+  
+  return postCategoriesTable;
+};
+  
+module.exports = postCategorySchema;
